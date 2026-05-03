54942afab6efc177def693442d7482295e62e1f4
Zehan <zehan.kareem@gmail.com>
2026-05-03 16:59:22 +0600

    types: add Level and Character interfaces for type safety
    - Create Character interface with image and name properties
    - Create Level interface with level, image, difficulty, and characters array
    - Export both interfaces for use across the application
    - Type the levels array as Level[]

---
+++ Frontend/src/data/levels.ts
@@ -0,0 +1,24 @@
+const levels: Level[] = [
+  {
+    level: "Level 1",
+    image: "/images/level1/image.png",
+    difficulty: "Medium",
+    characters: [
+      {
+        image: "/images/level1/characters/Walden.png",
+        name: "Walden",
+      },
+
+      {
+        image: "/images/level1/characters/Waldo.png",
+        name: "Waldo",
+      },
+      {
+        image: "/images/level1/characters/Wizard.png",
+        name: "Wizard",
+      },
+    ],
+  },
+];
+
+export default levels;
