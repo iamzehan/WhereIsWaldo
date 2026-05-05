546d170f8c0c68df3d07130f843c4063d012c227
Zehan <zehan.kareem@gmail.com>
2026-05-05 11:58:47 +0600

    feat: add initial results data structure for game levels

---
+++ Frontend/src/utils/results.ts
@@ -0,0 +1,22 @@
+export const results = [
+    {
+        level: 1,
+        results: [
+            {
+                name: "Walden",
+                x: 0.25,
+                y: 0.48
+            }, 
+            {
+                name: "Waldo",
+                x: 0.53,
+                y: 0.49
+            },
+            {
+                name: "Wizard",
+                x: 0.63,
+                y: 0.48
+            }
+        ]
+    }
+]
