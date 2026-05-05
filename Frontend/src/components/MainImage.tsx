96d2f8dc6cf28377c6bca123933e4e1355675ab5
Zehan <zehan.kareem@gmail.com>
2026-05-05 14:41:52 +0600

    feat: enhance MainImage component with character selection and click handling

--- Frontend/src/components/MainImage.tsx
+++ Frontend/src/components/MainImage.tsx
@@ -1,7 +1,19 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
+import CharactersDropDown from "./CharactersDropDown";
+import { useGame } from "../utils/hooks";
 
-export default function MainImage({ data }: { data: Level | null }) {
+type ClickPoint = {
+  x: number; // normalized (0–1)
+  y: number;
+};
+
+export default function MainImage() {
   const imgRef = useRef<HTMLImageElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [point, setPoint] = useState<ClickPoint | null>(null);
+  const [open, setOpen] = useState(false);
+
+  const {selected, setSelected, data} = useGame();
 
   const handleClick = (e: React.MouseEvent<HTMLImageElement>) => {
     if (!imgRef.current) return;
@@ -11,21 +23,44 @@ export default function MainImage({ data }: { data: Level | null }) {
     const rawX = (e.clientX - rect.left) / rect.width;
     const rawY = (e.clientY - rect.top) / rect.height;
 
-    // round to 2 decimal places (keep as number, not string)
     const x = Math.round(rawX * 100) / 100;
     const y = Math.round(rawY * 100) / 100;
+
+    setPoint({ x, y });
+    setOpen(true);
 
     console.log({ x, y });
   };
 
+  // Close on outside click
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      // if clicked target is not a node of the container then close the dropdown
+      if (!containerRef.current?.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => document.removeEventListener("mousedown", handleOutsideClick);
+  }, []);
+
   return (
-    <div className="w-full cursor-crosshair overflow-x-auto flex md:justify-center xl:justify-center">
+    // Wrapper
+    <div
+      ref={containerRef}
+      className="relative w-full flex md:justify-center xl:justify-center"
+    >
+      {/* Main Image */}
       <img
         ref={imgRef}
-        onClick={(e) => handleClick(e)}
+        onClick={handleClick}
         src={data?.image}
         alt={`${data?.level}_image`}
-        className="max-w-none md:w-5xl xl:w-7xl sm:w-full border-1 border-gray-200"
+        className="max-w-none md:w-5xl xl:w-7xl sm:w-full border border-gray-200 cursor-crosshair"
+      />
+      {/* Character matcher dropdown */}
+      <CharactersDropDown
+        props={{ open, point, data, selected, setSelected, setOpen }}
       />
     </div>
   );
