6657408402cd65decd9da78834ce7b0920f3bb1e
Zehan <zehan.kareem@gmail.com>
2026-05-05 14:42:15 +0600

    refactor: simplify Game component structure and remove unused state management

--- Frontend/src/pages/Game.tsx
+++ Frontend/src/pages/Game.tsx
@@ -1,31 +1,21 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getLevel } from "../utils/helper";
 import Leaderboard from "../components/LeaderBoard";
-import { useIsMobile } from "../utils/hooks";
 import MainImage from "../components/MainImage";
 import AvailableChars from "../components/AvailableCharacter";
+import { GameProvider } from "../utils/GameContext";
+import LevelCompletedDialog from "../components/LevelComplete";
 
 export default function Page() {
-  const [data, setData] = useState<Level | null>(null);
-  const { level } = useParams<string>();
-  const isMobile = useIsMobile();
-
-  useEffect(() => {
-    if (level) {
-      const levelData = getLevel(level);
-      setData(levelData || null);
-    }
-  }, [level]);
-
   return (
-    <div className="flex flex-col">
-      {/* Characters */}
-      <AvailableChars data={data} />
-      {/* Main Image */}
-      <MainImage data={data} />
-      {/* Leader board */}
-      <Leaderboard />
-    </div>
+    <GameProvider>
+      <div className="flex flex-col">
+        {/* Characters */}
+        <AvailableChars  />
+        {/* Main Image */}
+        <MainImage/>
+        {/* Leader board */}
+        <Leaderboard />
+      </div>
+      <LevelCompletedDialog/>
+    </GameProvider>
   );
 }
