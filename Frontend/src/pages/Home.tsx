8ac0a08e0499815bd0fb5cbc7b67c316c2e14313
Zehan <zehan.kareem@gmail.com>
2026-05-18 21:26:18 +0600

    fix(Home): update data fetching logic and error handling in Page component

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,10 +1,27 @@
-import levels from "../data/levels";
 import LevelCards from "../components/LevelCards";
+import { useQuery } from "@tanstack/react-query";
+import { getAllGames } from "../utils/requests.game";
+import { LevelCardsSkeleton } from "../components/Skeletons";
+import LevelCardsError from "../components/LevelCardsError";
 
 export default function Page() {
+  const {data, isLoading, error} = useQuery({
+    queryKey: ["games"],
+    queryFn: getAllGames,
+  });
+  if (isLoading) {
+    return <LevelCardsSkeleton/>
+  }
+
+  if (error) {
+    return <LevelCardsError/>
+  }
+
+  if(data)
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 w-[80vw] p-10 gap-2 w-screen">
-      <LevelCards levels={levels}/>
+      <LevelCards levels={data}/>
     </div>
   );
 }
+
