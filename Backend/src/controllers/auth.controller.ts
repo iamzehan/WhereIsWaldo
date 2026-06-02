784648f29081dadb24c12a3f7bf14da6d07d3c98
Zehan <zehan.kareem@gmail.com>
2026-06-02 11:54:00 +0600

    feat(auth): implement refresh token functionality and enhance error handling in registration

--- Backend/src/controllers/auth.controller.ts
+++ Backend/src/controllers/auth.controller.ts
@@ -2,10 +2,12 @@ import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
 import { hashPassword, comparePassword } from "../utils/password.js";
 import { signAccessToken, signRefreshToken } from "../utils/jwt.js";
+import jwt from 'jsonwebtoken';
+import {env} from '../config/env.js';
 
 export const register = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
-
+  try{
   const user = await prisma.user.create({
     data: {
       username,
@@ -13,8 +15,10 @@ export const register = async (req: Request, res: Response) => {
       password: await hashPassword(password)
     }
   });
-
   res.status(201).json({ id: user.id });
+} catch(err){
+  res.status(500).json({message: err});
+}
 };
 
 export const login = async (req: Request, res: Response) => {
@@ -31,5 +35,20 @@ export const login = async (req: Request, res: Response) => {
   req.session.userId = user.id;
   req.session.refreshToken = refreshToken;
 
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: (env.NODE_ENV==="development")? false : true,
+    sameSite: (env.NODE_ENV==="development")? "lax": "none",
+    path: "/api/auth/refresh"
+  });
+
   res.json({ accessToken });
 };
+
+export const refresh =  (req: Request, res: Response) => {
+  const token = req.cookies.refreshToken;
+  if (!token) return res.sendStatus(401);
+  const payload = jwt.verify(token, env.JWT_REFRESH_SECRET) as { sub: string };
+  const newAccessToken = signAccessToken(payload.sub);
+  res.json({ accessToken: newAccessToken });
+}
