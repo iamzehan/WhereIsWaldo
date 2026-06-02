7b17dd87ff053ce831c1155fab67e1d46b5eff63
Zehan <zehan.kareem@gmail.com>
2026-06-02 11:54:07 +0600

    fix(routes): add authentication middleware to game retrieval endpoint

--- Backend/src/routes/game.routes.ts
+++ Backend/src/routes/game.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as controller from "../controllers/game.controller.js";
+import { requireAuth } from "../middlewares/auth.middleware.js";
 
 
 const router = Router();
 
 router.get("/all", controller.getAllGames)
-router.get("/", controller.getOneGame);
+router.get("/", requireAuth, controller.getOneGame);
 
 export default router;
