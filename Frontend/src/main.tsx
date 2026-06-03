27271e86b492dae54acd850f0e6205402494db1c
Zehan <zehan.kareem@gmail.com>
2026-06-03 13:22:11 +0600

    refactor(main.tsx): comment out StrictMode for rendering

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+//import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -10,11 +10,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  // <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>,
+  //</StrictMode>
 );
