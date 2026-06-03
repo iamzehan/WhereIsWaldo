0e02efc58e02e969efc2e8d85b77038bb845d403
Zehan <zehan.kareem@gmail.com>
2026-06-03 13:22:34 +0600

    refactor(Home): reorganize imports and streamline loading/error handling

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,27 +1,33 @@
+import { useEffect } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getAllGames, deleteOpenLogs } from "../utils/requests.game";
 import LevelCards from "../components/LevelCards";
-import { useQuery } from "@tanstack/react-query";
-import { getAllGames } from "../utils/requests.game";
 import { LevelCardsSkeleton } from "../components/Skeletons";
 import LevelCardsError from "../components/LevelCardsError";
+import { useAuth } from "../utils/hooks";
 
 export default function Page() {
-  const {data, isLoading, error} = useQuery({
+  const { accessToken } = useAuth();
+  const { data, isLoading, error } = useQuery({
     queryKey: ["games"],
     queryFn: getAllGames,
   });
-  if (isLoading) {
-    return <LevelCardsSkeleton/>
-  }
 
-  if (error) {
-    return <LevelCardsError/>
-  }
+  const cleanupMutation = useMutation({
+    mutationFn: deleteOpenLogs,
+  });
 
-  if(data)
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-3 w-[80vw] p-10 gap-2 w-screen">
-      <LevelCards levels={data}/>
-    </div>
-  );
+  useEffect(() => {
+    cleanupMutation.mutate(accessToken);
+  }, []);
+
+  if (isLoading) return <LevelCardsSkeleton />;
+  if (error) return <LevelCardsError />;
+  if (data) {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-3 w-[80vw] p-10 gap-2 w-screen">
+        <LevelCards levels={data} />
+      </div>
+    );
+  }
 }
-
